842086f6a03a397fd365c545513a847459dce393
haraenkgowda-tech <haraenkgowda@gmail.com>
2026-04-26 09:26:59 +0530

    Lesson 34

---
+++ Lesson 33/script.js
@@ -0,0 +1,25 @@
+function clearScreen() { 
+    document.getElementById("result").value = "";  
+
+}  
+function setScreenValue(value) {
+    const r =document.getElementById("result");
+    if (r.value === "enter an expression" || r.value === "invalid expression") r.value
+    = "";
+    r.value += value;
+}
+function calculateResult() {
+    const resultElement = document.getElementById("result");
+    const expression = resultElement.trim();
+    if (expression === "") { 
+        resultElement.value = "enter an expression";
+        return;
+    }
+    try {
+        resultElement.value = eval(expression);
+    } catch (error) {
+        resultElement.value = "invalid expression";
+    }
+} 
+
+
